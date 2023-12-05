cc94efaefa866c0668deea6749c65f6256929493
Michael B <mbaklor@gmail.com>
2023-12-05 23:31:53 +0200

    day 1 part 2 works

--- d1.py
+++ d1.py
@@ -1,13 +1,43 @@
 input_file = "data-d1"
 
 
+def NumberWords(s: str) -> str:
+    wordMap = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+    if s[:5] in wordMap:
+        return wordMap[s[:5]]
+    if s[:4] in wordMap:
+        return wordMap[s[:4]]
+    if s[:3] in wordMap:
+        return wordMap[s[:3]]
+    return ""
+
+
 def FindEdgeNumber(s: str, inverted: bool) -> str:
     string = s
     if inverted:
         string = reversed(s)
-    for c in string:
+    for i, c in enumerate(string):
+        start = i
+        if inverted:
+            start = len(s) - 1 - i
         if c.isnumeric():
             return c
+        else:
+            if start + 3 > len(s):
+                continue
+            num = NumberWords(s[start:])
+            if num != "":
+                return num
     return ""
 
 
@@ -27,4 +57,5 @@ if __name__ == "__main__":
             combo = GetCombination(ln)
             sum += int(combo)
 
+    # sum = GetCombination("tnine123blahsevenhblah2one")
     print(sum)
