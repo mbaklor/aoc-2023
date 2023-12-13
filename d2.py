f52e2cc2fee505cd93961e795b3667f2f6eec934
Michael B <mbaklor@gmail.com>
2023-12-14 00:00:27 +0200

    day 2 part 2 works; learned dataclasses

--- d2.py
+++ d2.py
@@ -1,24 +1,42 @@
+from dataclasses import dataclass
+
+# input_file = "input"
 input_file = "data-d2"
 
-TARGET_GAME = {"red": 12, "green": 13, "blue": 14}
+
+@dataclass
+class dice:
+    red: int
+    green: int
+    blue: int
+
+    def __getitem__(self, color: str):
+        return self.__dict__[color]
+
+    def __setitem__(self, color: str, val):
+        self.__dict__[color] = val
+
+    def power(self) -> int:
+        return self.red * self.green * self.blue
 
 
-def checkGameWorks(game: str):
-    possible = True
+def checkGameMin(game: str) -> int:
     contents = game.split(";")
+    min_colors = dice(red=0, green=0, blue=0)
     for content in contents:
-        possible = possible & checkContentWorks(content.strip())
-    return possible
+        colors = checkContentMin(content.strip())
+        for name, _ in colors.__dict__.items():
+            min_colors[name] = max(colors[name], min_colors[name])
+    return min_colors.power()
 
 
-def checkContentWorks(content: str):
-    possible = True
+def checkContentMin(content: str) -> dice:
     each = content.split(",")
+    colors = dice(red=0, green=0, blue=0)
     for individual in each:
         color = individual.strip().split(" ")
-        if int(color[0]) > TARGET_GAME[color[1]]:
-            possible = False
-    return possible
+        colors[color[1]] = int(color[0])
+    return colors
 
 
 if __name__ == "__main__":
@@ -30,6 +48,5 @@ if __name__ == "__main__":
                 continue
             name, game = ln.split(":")
             gameID = int(name.split(" ")[1])
-            if checkGameWorks(game):
-                games += gameID
+            games += checkGameMin(game)
     print(games)
